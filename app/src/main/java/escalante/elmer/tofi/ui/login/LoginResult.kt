3c159c3f9da57c56b94bd9b18c9656b5b4931aa1
ITSONEDU\lv1013 <lab1013>
2022-04-05 19:15:28 -0700

    Login y colores

---
+++ app/src/main/java/escalante/elmer/tofi/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package escalante.elmer.tofi.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
---
+++ app/src/main/java/escalante/elmer/tofi/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package escalante.elmer.tofi.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import escalante.elmer.tofi.data.LoginDataSource
+import escalante.elmer.tofi.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
